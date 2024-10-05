e25126fc682d1fefcafe6d046e440a1d8ff297db
Sergey Malinin <smalinin@openlinksw.com>
2024-10-05 06:48:35 +0300

    Update version

--- llama_index_opal/__init__.py
+++ llama_index_opal/__init__.py
@@ -1,4 +1,4 @@
 from llama_index_opal.base import OPAL, OPAL_MODELS, OPAL_FUNCTIONS, OPAL_FINETUNES, OPAL_Assistant, get_OPAL_assistants_list
 
 __all__ = ["OPAL", "OPAL_MODELS", "OPAL_FUNCTIONS", "OPAL_FINETUNES", "OPAL_Assistant", "get_OPAL_assistants_list"]
-__version__ = "0.3.0"
+__version__ = "0.3.1"
